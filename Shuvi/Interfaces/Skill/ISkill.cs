fe06d9631165f3cf689781cc5732b662028a83b0
Sr. Mecha <sr.mecha@bk.ru>
2023-02-10 17:20:57 +0300

    Added skill interface

---
+++ Shuvi/Interfaces/Skill/ISkill.cs
@@ -0,0 +1,21 @@
+﻿using Shuvi.Interfaces.Combat;
+using Shuvi.Interfaces.Localization;
+using Shuvi.Interfaces.Spell;
+using Shuvi.Interfaces.Status;
+
+namespace Shuvi.Interfaces.Skill
+{
+    public interface ISkill
+    {
+        public interface ISkill
+        {
+            public ILocalizedInfo Info { get; }
+            public bool CanUse { get; }
+
+            public string GetStatus(ICombatEntity owner);
+            protected IActionResult OnSkillUse(ICombatEntity owner, ICombatEntity target);
+            public IActionResult UseSkill(ICombatEntity owner, ICombatEntity target);
+            public void Update(ICombatEntity owner);
+        }
+    }
+}
