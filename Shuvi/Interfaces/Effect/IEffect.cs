bd9cebf04a59910cbb2f4d88a3dc883ba24bb30a
Sr. Mecha <sr.mecha@bk.ru>
2023-02-10 17:21:08 +0300

    Added effect interfaces

---
+++ Shuvi/Interfaces/Effect/IEffect.cs
@@ -0,0 +1,14 @@
+﻿using Shuvi.Interfaces.Characteristics.Static;
+using Shuvi.Interfaces.Combat;
+
+namespace Shuvi.Interfaces.Effect
+{
+    public interface IEffect
+    {
+        public string Name { get; }
+        public int TimeLeft { get; }
+        public IStaticCharacteristics Bonuses { get; }
+        protected void OnUpdate(ICombatEntity target);
+        public void Update(ICombatEntity target);
+    }
+}
---
+++ Shuvi/Interfaces/Effect/IEffects.cs
@@ -0,0 +1,13 @@
+﻿using Shuvi.Interfaces.Combat;
+
+namespace Shuvi.Interfaces.Effect
+{
+    public interface IEffects
+    {
+        public List<IEffect> All { get; }
+
+        public void UpdateAll(ICombatEntity target);
+        public void Add(IEffect effect);
+        public void Remove(int index);
+    }
+}
