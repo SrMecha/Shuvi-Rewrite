b46919b49e04f118d4cdc1ad4f2ed7904c26b851
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:41:52 +0300

    Create CombatEnemy.cs

---
+++ Shuvi/Classes/Types/Combat/CombatEnemy.cs
@@ -0,0 +1,29 @@
+﻿using Shuvi.Classes.Types.Characteristics;
+using Shuvi.Classes.Types.Characteristics.Dynamic;
+using Shuvi.Enums.Localization;
+using Shuvi.Interfaces.Characteristics.Dynamic;
+using Shuvi.Interfaces.Combat;
+using Shuvi.Interfaces.Drop;
+using Shuvi.Interfaces.Enemy;
+
+namespace Shuvi.Classes.Types.Combat
+{
+    public class CombatEnemy : CombatEntity, ICombatEnemy
+    {
+        public int RatingGet { get; }
+        public IItemsDrop Drop { get; }
+
+        public CombatEnemy(IDatabaseEnemy enemy, Language lang)
+        {
+            Name = enemy.Info.GetName(lang);
+            Rank = enemy.Rank;
+            Characteristics = new EntityCharacteristics<INotRestorableCharacteristic>(enemy.Characteristics,
+                new NotRestorableCharacteristic(enemy.Characteristics.Health, enemy.Characteristics.Health),
+                new NotRestorableCharacteristic(enemy.Characteristics.Mana, enemy.Characteristics.Mana));
+            Spell = enemy.Spell.GetSpell();
+            Actions = enemy.ActionChances;
+            RatingGet = enemy.RatingGet;
+            Drop = enemy.Drop;
+        }
+    }
+}
