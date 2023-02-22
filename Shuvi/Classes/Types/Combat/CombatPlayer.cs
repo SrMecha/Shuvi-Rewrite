ce12fd2d8eb9cc05773388342b35f7066d05b76e
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:34:59 +0300

    Modify constructor

--- Shuvi/Classes/Types/Combat/CombatPlayer.cs
+++ Shuvi/Classes/Types/Combat/CombatPlayer.cs
@@ -1,5 +1,8 @@
-﻿using Shuvi.Classes.Types.Inventory;
+﻿using Shuvi.Classes.Types.Characteristics.Dynamic;
+using Shuvi.Classes.Types.Characteristics;
+using Shuvi.Classes.Types.Inventory;
 using Shuvi.Enums.Localization;
+using Shuvi.Interfaces.Characteristics.Dynamic;
 using Shuvi.Interfaces.Combat;
 using Shuvi.Interfaces.Inventory;
 using Shuvi.Interfaces.Skill;
@@ -13,9 +16,16 @@ namespace Shuvi.Classes.Types.Combat
         public ISkill Skill { get; private set; }
         public IDropInventory Inventory { get; private set; }
 
-        public CombatPlayer(IDatabaseUser user) 
+        public CombatPlayer(IDatabaseUser user, string name) 
         {
+            Name = name;
+            Rank = user.Rating.Rank;
+            Characteristics = new EntityCharacteristics<INotRestorableCharacteristic>(user.Characteristic,
+                new NotRestorableCharacteristic(user.Characteristic.Health.GetCurrent(), user.Characteristic.Health.GetCurrent()),
+                new NotRestorableCharacteristic(user.Characteristic.Mana.GetCurrent(), user.Characteristic.Mana.GetCurrent()));
+            Spell = user.Spell.GetSpell();
             Skill = user.Skill.GetSkill();
+            Actions = user.ActionChances;
             Inventory = new DropInventory();
         }
         public IActionResult UseSkill(ICombatEntity target, Language lang)
