86b3b064d9ee9ca2c3bdb58b62686ea8818fc75a
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:16:17 +0300

    Create ResultStorage.cs

---
+++ Shuvi/Classes/Types/Status/ResultStorage.cs
@@ -0,0 +1,24 @@
+﻿using Shuvi.Interfaces.Status;
+
+namespace Shuvi.Classes.Types.Status
+{
+    public class ResultStorage : IResultStorage
+    {
+        public List<IActionResult> Results { get; protected set; } = new();
+
+
+        public void Add(IActionResult? result)
+        {
+            if (result is not null)
+                Results.Add(result);
+        }
+        public void Clear()
+        {
+            Results.Clear();
+        }
+        public string GetDescriptions()
+        {
+            return string.Join("\n", Results);
+        }
+    }
+}
