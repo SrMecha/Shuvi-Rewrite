9e92a925c3df5f30abde691a74d587badc93c3f4
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:25:31 +0300

    Interface fixes

--- Shuvi/Interfaces/Combat/ICombatPlayer.cs
+++ Shuvi/Interfaces/Combat/ICombatPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Shuvi.Interfaces.Inventory;
+﻿using Shuvi.Enums.Localization;
+using Shuvi.Interfaces.Inventory;
 using Shuvi.Interfaces.Skill;
 using Shuvi.Interfaces.Spell;
 using Shuvi.Interfaces.Status;
@@ -8,7 +9,7 @@ namespace Shuvi.Interfaces.Combat
     public interface ICombatPlayer
     {
         public ISkill Skill { get; }
-        public IUserInventory Inventory { get; }
-        public IActionResult UseSkill(ICombatEntity target);
+        public IDropInventory Inventory { get; }
+        public IActionResult UseSkill(ICombatEntity target, Language lang);
     }
 }
