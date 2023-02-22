b42d475b6f5f8d1c4b57fafabb973bcc79f2245d
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 18:57:11 +0300

    Create VoidSkill.cs

---
+++ Shuvi/Classes/Types/Skill/SkillList/VoidSkill.cs
@@ -0,0 +1,12 @@
+﻿using Shuvi.Interfaces.Combat;
+
+namespace Shuvi.Classes.Types.Skill.SkillList
+{
+    public class VoidSkill : SkillBase
+    {
+        public override bool CanUse(ICombatEntity owner)
+        {
+            return false;
+        }
+    }
+}
