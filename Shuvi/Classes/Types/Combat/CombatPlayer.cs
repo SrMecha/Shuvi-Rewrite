2d257f177948c17b7649ca3b42b171e2a8607404
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:25:55 +0300

    Complite CombatPlayer

--- Shuvi/Classes/Types/Combat/CombatPlayer.cs
+++ Shuvi/Classes/Types/Combat/CombatPlayer.cs
@@ -1,4 +1,6 @@
-﻿using Shuvi.Interfaces.Combat;
+﻿using Shuvi.Classes.Types.Inventory;
+using Shuvi.Enums.Localization;
+using Shuvi.Interfaces.Combat;
 using Shuvi.Interfaces.Inventory;
 using Shuvi.Interfaces.Skill;
 using Shuvi.Interfaces.Status;
@@ -9,15 +11,22 @@ namespace Shuvi.Classes.Types.Combat
     public class CombatPlayer : CombatEntity, ICombatPlayer
     {
         public ISkill Skill { get; private set; }
-        public IUserInventory Inventory { get; private set; }
+        public IDropInventory Inventory { get; private set; }
 
         public CombatPlayer(IDatabaseUser user) 
-        { 
-
-        }
-        public IActionResult UseSkill(ICombatEntity target)
         {
-            throw new NotImplementedException();
+            Skill = user.Skill.GetSkill();
+            Inventory = new DropInventory();
+        }
+        public IActionResult UseSkill(ICombatEntity target, Language lang)
+        {
+            return Skill.UseSkill(this, target, lang);
+        }
+        public override IResultStorage Update(Language lang)
+        {
+            var result =  base.Update(lang);
+            result.Add(Skill.Update(this, lang));
+            return result;
         }
     }
 }
