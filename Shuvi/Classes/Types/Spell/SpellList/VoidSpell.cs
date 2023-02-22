6acb16f65521158376520abbdce0adc824775172
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 18:50:39 +0300

    Removed constructor

--- Shuvi/Classes/Types/Spell/SpellList/VoidSpell.cs
+++ Shuvi/Classes/Types/Spell/SpellList/VoidSpell.cs
@@ -4,7 +4,6 @@ namespace Shuvi.Classes.Types.Spell.SpellList
 {
     public class VoidSpell : SpellBase
     {
-        public VoidSpell() { }
         public override bool CanCast(ICombatEntity player)
         {
             return false;
