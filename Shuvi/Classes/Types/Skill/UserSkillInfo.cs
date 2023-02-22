2346a520de611f9119f81235901a1c458e19e8c5
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 19:04:24 +0300

    Create UserSkillInfo.cs

---
+++ Shuvi/Classes/Types/Skill/UserSkillInfo.cs
@@ -0,0 +1,16 @@
+﻿using Shuvi.Classes.Types.Localization;
+using Shuvi.Classes.Types.Spell.SpellList;
+using Shuvi.Interfaces.Skill;
+
+namespace Shuvi.Classes.Types.Skill
+{
+    public class UserSkillInfo : SkillInfo, IUserSkillInfo
+    {
+        public UserSkillInfo(string? skillName) : base(skillName) { }
+        public void SetSkill(string? skillName)
+        {
+            _skillName = skillName;
+            Info = new CachedLocalizedInfo("spells", skillName);
+        }
+    }
+}
