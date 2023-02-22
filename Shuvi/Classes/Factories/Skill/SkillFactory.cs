8a67005cf4bda1dd8434c590d98e5ac50c3fbdb5
Sr. Mecha <sr.mecha@bk.ru>
2023-02-22 18:52:14 +0300

    Added skill factory

--- Shuvi/Classes/Factories/Skill/SkillFactory.cs
+++ Shuvi/Classes/Factories/Skill/SkillFactory.cs
@@ -1,9 +1,22 @@
-﻿using Shuvi.Interfaces.Skill;
+﻿using Shuvi.Classes.Types.Skill.SkillList;
+using Shuvi.Interfaces.Skill;
 
 namespace Shuvi.Classes.Factories.Skill
 {
     public static class SkillFactory
     {
-        public static ISkill CreateSkill(UserRa)
+        private static IReadOnlyDictionary<string, ISkill>? _skills;
+
+        public static void SetDictionary(IReadOnlyDictionary<string, ISkill> skills)
+        {
+            _skills = skills;
+        }
+
+        public static ISkill GetSpell(string skillName)
+        {
+            if (_skills!.TryGetValue(skillName, out var result))
+                return result.CreateCopy();
+            return new VoidSkill();
+        }
     }
 }
