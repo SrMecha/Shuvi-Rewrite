5832234ca74dfc116f57cf60ce23f2ce565d5646
Sr. Mecha <sr.mecha@bk.ru>
2023-02-23 18:29:35 +0300

    Create CombatPet.cs

---
+++ Shuvi/Classes/Types/Combat/CombatPet.cs
@@ -0,0 +1,22 @@
+﻿using Shuvi.Classes.Types.Characteristics.Dynamic;
+using Shuvi.Classes.Types.Characteristics;
+using Shuvi.Classes.Types.Pet;
+using Shuvi.Interfaces.Characteristics.Dynamic;
+using Shuvi.Interfaces.Combat;
+
+namespace Shuvi.Classes.Types.Combat
+{
+    public class CombatPet : CombatEntity, ICombatPet
+    {
+        public CombatPet(DatabasePet pet)
+        {
+            Name = pet.Name;
+            Rank = pet.Rank;
+            Characteristics = new EntityCharacteristics<INotRestorableCharacteristic>(pet.Characteristics,
+            new NotRestorableCharacteristic(pet.Characteristics.Health.GetCurrent(), pet.Characteristics.Health.GetCurrent()),
+                new NotRestorableCharacteristic(pet.Characteristics.Mana.GetCurrent(), pet.Characteristics.Mana.GetCurrent()));
+            Spell = pet.Spell.GetSpell();
+            Actions = pet.ActionChances;
+        }
+    }
+}
