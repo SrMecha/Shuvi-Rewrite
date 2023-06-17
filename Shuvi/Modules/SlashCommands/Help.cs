d68f63d56374b9fd7593e733eb64ed93e840494d
Sr. Mecha <sr.mecha@bk.ru>
2023-06-17 18:37:12 +0300

    Added help command

---
+++ Shuvi/Modules/SlashCommands/Help.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Shuvi.Classes.Types.Interaction;
+using Shuvi.CommandParts;
+using Shuvi.Services.StaticServices.Database;
+using Shuvi.Services.StaticServices.Localization;
+
+namespace Shuvi.Modules.SlashCommands
+{
+    public class HelpCommandModule : InteractionModuleBase<CustomInteractionContext>
+    {
+        private readonly LocalizationLanguagePart _errorPart = LocalizationService.Get("errorPart");
+        private readonly DiscordShardedClient _client;
+
+        public HelpCommandModule(IServiceProvider provider)
+        {
+            _client = provider.GetRequiredService<DiscordShardedClient>();
+        }
+
+        [SlashCommand("help", "Посмотреть список команд.")]
+        public async Task InfoCommandAsync()
+        {
+            await DeferAsync();
+            var dbUser = await UserDatabase.TryGetUser(Context.User.Id);
+            if (dbUser is null)
+            {
+                await AccountCreatePart.Start(Context);
+                return;
+            }
+            await HelpPart.Start(Context, dbUser);
+        }
+    }
+}
